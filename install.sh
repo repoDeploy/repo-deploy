38283c1dc7670dc87b7d6e1dc702423fb9e8f440
thomas10-10 <36126914+thomas10-10@users.noreply.github.com>
2021-11-17 15:42:09 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,3 @@
+cd /opt  
+sudo curl -L "https://github.com/thomas10-10/repo-deploy/releases/download/$(basename $(curl -fsSLI -o /dev/null -w %{url_effective} https://github.com/thomas10-10/repo-deploy/releases/latest))/rd.tar.gz" | sudo tar -xz 
+sudo ln -s /opt/rd/rd /usr/local/bin/rd
