21f4fe971a3eb699d1247d9995384e0613a7f0de
thomas10-10 <36126914+thomas10-10@users.noreply.github.com>
2022-02-26 14:04:11 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #Install
 sudo curl -Lo /usr/local/bin/rd "https://github.com/repoDeploy/repo-deploy/releases/download/$(basename $(curl -fsSLI -o /dev/null -w %{url_effective} https://github.com/repoDeploy/repo-deploy/releases/latest))/rd" 
-
+sudo chmod +x /usr/local/bin/rd
 #old
 #cd /opt  
 #sudo curl -L "https://github.com/repoDeploy/repo-deploy/releases/download/$(basename $(curl -fsSLI -o /dev/null -w %{url_effective} https://github.com/repoDeploy/repo-deploy/releases/latest))/rd.tar.gz" | sudo tar -xz 
